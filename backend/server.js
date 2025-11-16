41a11b1f99b309697df430ee00ab78d407a44b52
cb-here <cbv934@gmail.com>
2025-11-16 12:56:16 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -9,19 +9,7 @@ const allowedOrigins = (
   process.env.ALLOWED_DOMAINS || "http://localhost:3000"
 ).split(",");
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, 
-  })
-);
+app.use(cors());
 
 app.use(express.json({ limit: "1gb" }));
 app.use(express.urlencoded({ limit: "1gb", extended: true }));
